c2892e8617e9a2babdacc1046d478a47c13b29aa
mahshid <mahshidhdi@gmail.com>
2017-01-24 11:28:27 +0330

    number is set

--- crawler/crawler/spiders/wiki_link_spider.py
+++ crawler/crawler/spiders/wiki_link_spider.py
@@ -2,12 +2,11 @@ import scrapy
 from bs4 import BeautifulSoup
 from scrapy import statscollectors
 from scrapy.exceptions import CloseSpider
-from scrapy.spiders import BaseSpider
 
 
-class WikiLinkSpider(BaseSpider):
+class WikiLinkSpider(scrapy.Spider):
     name = "wikilinks"
-    COUNT_MAX = 50  #TODO : 1000 pishfarze
+    COUNT_MAX = 100  # TODO : 1000 pishfarze
     OUT_MAX = 10
     count = 0
 
@@ -18,17 +17,11 @@ class WikiLinkSpider(BaseSpider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    # def parse(self, response):
-    #     page = response.url.split("/")[-2]
-    #     filename = 'quotes-%s.html' % page
-    #     with open(filename, 'wb') as f:
-    #         f.write(response.body)
-    #     self.log('Saved file %s' % filename)
-
     def parse(self, response):
-        # use lxml to get decent HTML parsing speed
-        print("stats:", self.crawler.stats.get_stats()['response_received_count'])
-        if self.crawler.stats.get_stats()['response_received_count'] >= self.COUNT_MAX:
+        # print("stats:", self.crawler.stats.get_stats()['response_received_count'])
+        # print("stats:", self.crawler.stats.get_stats())
+        if self.crawler.stats.get_stats()['response_received_count'] >= self.COUNT_MAX and \
+                        self.crawler.stats.get_stats()['item_scraped_count'] >= self.COUNT_MAX:
             raise CloseSpider(reason="API usage exceeded")
 
         soup = BeautifulSoup(response.text, 'lxml')
@@ -39,10 +32,12 @@ class WikiLinkSpider(BaseSpider):
         for content_text in soup.find_all(attrs={'id': 'mw-content-text'}):
             paragraphs = content_text.find_all('p')
             abstract = paragraphs[0].get_text()
-            # print ("intro: ", abstract)
+            print ("abstract: ", abstract)
             for parag in paragraphs:
                 if parag.parent.get('id') == 'mw-content-text':
-                    print("parent of p: ", [parent for parent in parag.parents])
+
+
+                    # print("parent of p: ", [parent.get('id') for parent in parag.parents])
                     main_text += (' ' + parag.get_text())
             yield {
                 'title': title,
@@ -52,30 +47,15 @@ class WikiLinkSpider(BaseSpider):
 
             # finding links
             out_degree = 0
-            for link in content_text.find_all('a'):   #TODO: change to input parameter
+            for link in content_text.find_all('a'):  # TODO: change to input parameter
                 # print("parent of link: ", [parent.name for parent in link.parents])
                 # print("count " ,  self.count)
                 if link.parent.name == 'p':
                     next_page = link.get('href')
                     self.count += 1
-                    out_degree +=1
+                    out_degree += 1
                     if self.crawler.stats.get_stats()['response_received_count'] < self.COUNT_MAX:
                         yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
                     if out_degree >= self.OUT_MAX:
                         break
-
-
-
-#
-# class BlogSpider(scrapy.Spider):
-#     name = 'blogspider'
-#     start_urls = ['https://blog.scrapinghub.com']
-#
-#     def parse(self, response):
-#         for title in response.css('h2.entry-title'):
-#             yield {'title': title.css('a ::text').extract_first()}
-#
-#         next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
-#         if next_page:
-#             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)

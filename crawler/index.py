79048068270d83edf2eae974a86eec6bdc49627a
Apple <Apple@Apples-iMac-2.local>
2017-01-25 19:20:58 +0330

    p matrix built

--- crawler/index.py
+++ crawler/index.py
@@ -3,10 +3,11 @@ import os
 
 import requests
 
-from constants import ES_CLIENT,INDEX_NAME
-
-
 # start elastic server
+from constants import INDEX_NAME, ES_CLIENT
+
+MAP_URL_TO_ID={}
+MAP_ID_TO_URL={}
 def make_index():
     r = requests.get('http://localhost:9200')
     doc_id = 1
@@ -15,13 +16,19 @@ def make_index():
     delet_index()
     ES_CLIENT.indices.create(index=INDEX_NAME)##TODO :shards=1
     file_list = os.listdir("json_files/")
-    print("file lists:", file_list)
+    # print("file lists:", file_list)
     for file in file_list:
         with open("json_files/" + file, 'r') as f:
-            ES_CLIENT.index(index=INDEX_NAME, doc_type='article', id=doc_id, body=json.load(f))
+            jfile=json.load(f)
+            # print("the content of json:",jfile)
+            ES_CLIENT.index(index=INDEX_NAME, doc_type='article', id=doc_id, body=jfile,ignore=400)
             print("index is build " + str(doc_id))
-            print(ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id))
+            # print(ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id , ignore=[400,404]))
+            MAP_ID_TO_URL[doc_id]=jfile["curr_link"]
+            MAP_URL_TO_ID[jfile["curr_link"]]=doc_id
+
             doc_id = doc_id + 1
+
     return doc_id-1
 
 #deleting index
@@ -30,8 +37,6 @@ def delet_index():
 
 
 TOTAL_DOC_NUMBER = make_index()
-#page rank
-
 
 
 
--- crawler/pagerank.py
+++ crawler/pagerank.py
@@ -1,2 +1,21 @@
+from index import *
+from numpy import linalg as LA
+p=[]
+def build_pmatrix (alpha ):
+    docIDs_list= MAP_ID_TO_URL.keys()
+    V=float(1/TOTAL_DOC_NUMBER)
+    for doc_id_row in docIDs_list:
+        p[doc_id_row]=[]
+        out_degrees=ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id_row , ignore=[400,404])["source"]["out_links"]
+        for doc_id_col in docIDs_list:
+            if not out_degrees:
+                p[doc_id_row][doc_id_col]=alpha*V
+            else:
+                if doc_id_col in out_degrees:
+                    p[doc_id_row][doc_id_col]=(1-alpha)*(float(1/len(out_degrees)))+alpha*V
+                else:
+                    p[doc_id_row][doc_id_col]=0
 
-
+def find_eigenvector():
+    w, v = LA.eig(p)
+    print("eigen vector of p is:",v)

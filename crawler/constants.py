65a197e9e2dd0b7098440da8ed704861cb7aa406
mahshid <mahshidhdi@gmail.com>
2017-01-25 16:55:56 +0330

    refactor

---
+++ crawler/constants.py
@@ -0,0 +1,4 @@
+from elasticsearch import Elasticsearch
+
+ES_CLIENT = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+INDEX_NAME = "wiki_index"
--- crawler/index.py
+++ crawler/index.py
@@ -1,8 +1,10 @@
+import json
 import os
 
-from elasticsearch import Elasticsearch
 import requests
-import json
+
+from constants import ES_CLIENT,INDEX_NAME
+
 
 # start elastic server
 def make_index():
@@ -11,26 +13,23 @@ def make_index():
 
     # indexing every jsons
     delet_index()
-    elastic_search_instance.indices.create(index='wiki_index')##TODO :shards=1
+    ES_CLIENT.indices.create(index=INDEX_NAME)##TODO :shards=1
     file_list = os.listdir("json_files/")
     print("file lists:", file_list)
     for file in file_list:
         with open("json_files/" + file, 'r') as f:
-            elastic_search_instance.index(index='wiki_index', doc_type='article', id=doc_id, body=json.load(f))
+            ES_CLIENT.index(index=INDEX_NAME, doc_type='article', id=doc_id, body=json.load(f))
             print("index is build " + str(doc_id))
-            print(elastic_search_instance.get(index='wiki_index', doc_type='article', id=doc_id))
+            print(ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id))
             doc_id = doc_id + 1
-
-
-#print(es.search(index="wiki_index", body={"query": {"match": {'main_text': 'سعدی'}}}))
+    return doc_id-1
 
 #deleting index
 def delet_index():
-    elastic_search_instance.indices.delete(index="wiki_index", ignore=[400, 404])
+    ES_CLIENT.indices.delete(index=INDEX_NAME, ignore=[400, 404])
 
-total_doc_number=id -1
-elastic_search_instance = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-make_index()
+
+TOTAL_DOC_NUMBER = make_index()
 #page rank
 
 

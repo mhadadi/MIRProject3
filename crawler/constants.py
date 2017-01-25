277e327e344a62b76a11a60afd2743ffa2f06c64
mahshid <mahshidhdi@gmail.com>
2017-01-25 19:19:11 +0330

    tf matrix

--- crawler/constants.py
+++ crawler/constants.py
@@ -1,4 +1,13 @@
 from elasticsearch import Elasticsearch
 
-ES_CLIENT = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+ES_CLIENT = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}], timeout=180)
 INDEX_NAME = "wiki_index"
+MAIN_TEXT = "main_text"
+TITLE = "title"
+INFO_BOX = "info_box"
+ABSTRACT = "abstract"
+CURR_LINK = "curr_link"
+OUT_LINKS = "out_links"
+DEFAULT_TYPE = "article"
+
+VOCAB = []
--- crawler/index.py
+++ crawler/index.py
@@ -2,13 +2,14 @@ import json
 import os
 
 import requests
+from twisted.conch.scripts.cftp import _ignore
 
 from constants import ES_CLIENT,INDEX_NAME
 
 
 # start elastic server
 def make_index():
-    r = requests.get('http://localhost:9200')
+    # r = requests.get('http://localhost:9200')
     doc_id = 1
 
     # indexing every jsons
@@ -18,9 +19,9 @@ def make_index():
     print("file lists:", file_list)
     for file in file_list:
         with open("json_files/" + file, 'r') as f:
-            ES_CLIENT.index(index=INDEX_NAME, doc_type='article', id=doc_id, body=json.load(f))
-            print("index is build " + str(doc_id))
-            print(ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id))
+            print("index is being build " + str(doc_id) + str(file))
+            ES_CLIENT.index(index=INDEX_NAME, doc_type='article', id=doc_id, body=json.load(f), ignore=400)
+            print(ES_CLIENT.get(index=INDEX_NAME, doc_type='article', id=doc_id, ignore=404))
             doc_id = doc_id + 1
     return doc_id-1
 
